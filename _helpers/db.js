4be28961c5679c47a933eb8cb344453e53b4f3e3
MaglangitNino <cokaliongpirate03@gmail.com>
2024-04-02 22:39:19 +0800

    first commit

---
+++ _helpers/db.js
@@ -0,0 +1,29 @@
+const config = require('config.json');
+const mysql = require('mysql2/promise');
+const sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+
+    const {host, port, user, password, database} = config.database;
+    const connection = await mysql.createConnection({host, port, user, password});
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+
+    const sequelize = new Sequelize(database, user, password, {dialect: 'mysql'});
+
+    db.Account = require('../accounts/account.model')(sequelize);
+    db.RefreshToken = require('../accounts/RefreshToken.model')(sequelize);
+
+
+    db.Account.hashMany(db.RefreshToken, {onDelete: 'CASCADE'});
+    db.RefreshToken.belongsTo(db.Account);
+
+
+    await sequelize.sync();
+}  
+
